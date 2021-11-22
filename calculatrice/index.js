56f2d6c38d100098a96c141370777cfa2b873888
BladeRED <pokeblade43@gmail.com>
2021-11-22 14:28:31 +0100

    MAJ_Exo_Date

--- calculatrice/index.js
+++ calculatrice/index.js
@@ -1,16 +1,24 @@
 "use strict";
-
-
-
-const tauxTVA = 20 ;
+const TAUX_TVA = 20;
 
 let montantHT;
+let tauxTVA;
+let montantTTC;
 
-montantHT = prompt("Veuillez saisir le montant Hors-Taxe, s'il vous plaît") ;
+montantHT = prompt("Quel est le montant HT?");
+montantHT = parseFloat(montantHT);
+console.log(montantHT);
 
-let montantTTC = montantHT * tauxTVA/100;
+tauxTVA = montantHT*(TAUX_TVA/100);
+console.log(tauxTVA);
+montantTTC = montantHT + tauxTVA;
+console.log(montantTTC);
 
+const HT = document.querySelector("#montantHT");
+HT.textContent = montantHT;
 
+const TVA = document.querySelector("#tauxTVA");
+TVA.textContent = tauxTVA;
 
-
-console.log (`Le montant Hors-Taxe étant de ${montantHT} et le Taux de TVA étant de ${tauxTVA}, le montant TTC est de ${montantTTC}`)
+const TTC = document.querySelector("#montantTTC");
+TTC.textContent = montantTTC;
