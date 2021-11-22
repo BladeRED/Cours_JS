d68e69d1491669b395cc9e2c13b76d0ccadb39b7
BladeRED <pokeblade43@gmail.com>
2021-11-22 16:50:44 +0100

    Corrections

--- date_du_jour/main.js
+++ date_du_jour/main.js
@@ -2,5 +2,56 @@
 
 //déclaration de la variable
 
-var nomDeLaVariable;
-let autreVariable;
+let weekDays = [
+
+"dimanche",
+"lundi",
+"mardi",
+"mercredi",
+"judi",
+"vendredi",
+"samedi",
+] ;
+
+console.log (weekDays);
+
+// crée un objet Date
+
+let today = new Date ();
+
+//récupérer le jour de la semaine en cours
+
+let dayIndex = today.getDay();
+console.log (weekDays[dayIndex]);
+
+let date = today.getDate();
+console.log(date)
+
+let monthNow = [
+
+"Janvier",
+"Février",
+"Mars",
+"Avril",
+"Mai",
+"Juin",
+"Juillet",
+"Août",
+"Septembre",
+"Octobre" ,
+"Novembre",
+"Décembre" ,
+] ;
+
+let month = new Date ("2021/11/23");
+
+let monthIndex = month.getMonth();
+console.log (monthNow[monthIndex]);
+
+let year = today.getFullYear();
+console.log (year);
+
+
+
+
+
