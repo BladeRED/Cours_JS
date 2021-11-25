57a1db515c54913501fbfe333f5a71e6d4ecc224
BladeRED <pokeblade43@gmail.com>
2021-11-25 20:36:13 +0100

    Update main.js

--- photo/main.js
+++ photo/main.js
@@ -2,6 +2,7 @@
 
 let count = 0;
 
+
 // FONCTIONS //
 
 function selectOne() {
@@ -16,8 +17,25 @@ function selectOne() {
 
 }
 
+function selectAll(){
+
+    photos.forEach((li) => li.classList.add("selected"));
+    let selected = document.querySelectorAll("li.selected");
+    document.querySelector("#total em").textContent = selected.length;
+    console.log("ca marche");
+    
+}
+
+function deselectAll(){
+
+    photos.forEach((li) => li.classList.remove("selected"));
+    count = 0;
+    document.querySelector("#total em").textContent = count;
+}
+
 // CODE PRINCIPAL //
 
 const photos = document.querySelectorAll(".photo-list li");
 photos.forEach((li) => li.addEventListener("click", selectOne));
-
+let selectButton = document.querySelector("#selectAll").addEventListener("click", selectAll);
+let deselectButton = document.querySelector("#deselectAll").addEventListener("click", deselectAll);
