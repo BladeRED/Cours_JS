6c0fcd749e6cac401f68ec9a6487a928f30e5050
BladeRED <pokeblade43@gmail.com>
2021-11-26 10:29:25 +0100

    Update main.js

--- slider/js/main.js
+++ slider/js/main.js
@@ -1,1 +1,43 @@
 "use strict";
+
+//VARIABLES //
+
+/*let imgObject = {
+
+image = document.querySelector("img src"),
+legend = document.querySelector("img alt"),
+
+
+} */
+
+let state = {
+
+index = 0,
+launched = false,
+
+
+};
+
+let slides = [
+
+{image:"1.jpg", legend: "panda"},
+{image:"2.jpg", legend: "lapin"},
+{image:"3.jpg", legend: "chat"},
+{image:"4.jpg", legend: "dauphin"},
+{image:"5.jpg", legend: "tourterelle"},
+{image:"6.jpg", legend: "ours"},
+
+];
+
+
+for (let i = 0; i < slides.length; i++){
+
+    console.log(slides[i]);
+}
+
+// FONCTIONS //
+
+
+
+
+// CODE PRINCIPAL //
